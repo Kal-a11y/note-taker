111d5ef284a1baa0010311808381581b9f7b1b2f
Kaila <kailaamia@gmail.com>
2023-12-04 23:30:17 -0600

    Undo test fix

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
 
 // //Open index.html in root
-// app.get('/', (req,res) => {
-//     res.sendFile(path.join(__dirname,'public/index.html'));
-// });
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname,'public/index.html'));
+});
 
 //Open notes.html 
 app.get('/notes', (req,res) => {
